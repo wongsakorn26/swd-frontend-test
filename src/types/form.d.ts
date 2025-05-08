7cc34604763533263bb91c1a2824daf6375f5d94
Wongsakorn <wongsakornhomsung@gmail.com>
2025-05-08 16:31:55 +0700

    [5][FEAT]form done

---
+++ src/types/form.d.ts
@@ -0,0 +1,17 @@
+export type userFormProps = {
+    title: string
+    firstName: string
+    lastName: string
+    birthDay: string
+    nationality: string
+    citizenId: string
+    gender: string
+    mobilePhone: string
+    passportNo: string
+    expectedSalary: string
+    citizenIdP1: string
+    citizenIdP2: string
+    citizenIdP3: string
+    citizenIdP4: string
+    citizenIdP5: string
+}
